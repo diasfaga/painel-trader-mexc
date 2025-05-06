b8b55c38f543088944b9331a59873a31e63c2945
diasfaga <faga05@gmail.com>
2025-05-06 20:12:17 +0200

    Atualiza├º├úo autom├ítica

--- enviar_git_redeploy.bat
+++ enviar_git_redeploy.bat
@@ -10,7 +10,9 @@ git push origin main
 echo ================================
 echo Chamando redeploy no Railway...
 
-curl -X POST "https://backboard.railway.app/project/0e4e956f-18cd-4bdc-bc1d-a8f7d8ae323e/deploy" -H "Authorization: e1f211bd-cda2-453b-88ff-fbeb778f9fb1"
+curl -X POST https://backboard.railway.app/project/0e4e956f-18cd-4bdc-bc1d-a8f7d8ae323e/deploy?serviceId=ed87f2ad-8de3-4d39-8ec3-9f6a72d28cee ^
+-H "Authorization: Bearer e1f211bd-cda2-453b-88ff-fbeb778f9fb1"
+
 
 echo ================================
 echo Redeploy Railway chamado!
