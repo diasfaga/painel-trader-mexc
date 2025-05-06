27f593aca4131ba74be699ef2f173de9b17e2457
diasfaga <faga05@gmail.com>
2025-05-07 00:10:52 +0200

    app.py

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
 from flask import Flask
 
+import os
+
 app = Flask(__name__)
 
-@app.route('/')
+@app.route("/")
 def home():
-    return '✅ Aplicação Flask mínima no Railway funcionando!'
+    return "✅ Aplicação Flask mínima no Railway funcionando!"
 
-if __name__ == '__main__':
-    app.run()
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
